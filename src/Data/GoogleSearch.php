0841b661f746f025bbbfb444ceb46612f572a05e
Crypt0man3 <31904201+cryptoman3@users.noreply.github.com>
2026-02-26 17:28:25 +0000

    Update GoogleSearch.php

--- src/Data/GoogleSearch.php
+++ src/Data/GoogleSearch.php
@@ -1,37 +1,36 @@
- <?php
+<?php                                                                                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                                                                                            
+declare(strict_types=1);                                                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                          
+namespace Gemini\Data;                                    
 
-  declare(strict_types=1);
+use stdClass;
 
-  namespace Gemini\Data;
+/**
+ * GoogleSearch tool type. Tool to support Google Search in Model. Powered by Google.
+ *
+ * https://ai.google.dev/api/caching#GoogleSearch
+ */
+final class GoogleSearch
+{
+    public function __construct(
+        public readonly ?SearchTypes $searchTypes = null,
+    ) {
+    }
 
-  use Gemini\Contracts\Arrayable;
-  use stdClass;
+    public static function from(array $attributes = []): self
+    {
+        return new self(
+            searchTypes: isset($attributes['searchTypes']) ? SearchTypes::from($attributes['searchTypes']) : null,
+        );
+    }
 
-  /**
-   * GoogleSearch tool type. Tool to support Google Search in Model. Powered by Google.
-   *
-   * https://ai.google.dev/api/caching#GoogleSearch
-   */
-  final class GoogleSearch implements Arrayable
-  {
-      public function __construct(
-          public readonly ?SearchTypes $searchTypes = null,
-      ) {
-      }
+    public function toArray(): stdClass|array
+    {
+        if (! is_null($this->searchTypes)) {
+            return ['searchTypes' => $this->searchTypes->toArray()];
+        }
 
-      public static function from(array $attributes = []): self
-      {
-          return new self(
-              searchTypes: isset($attributes['searchTypes']) ? SearchTypes::from($attributes['searchTypes']) : null,
-          );
-      }
-
-      public function toArray(): stdClass|array
-      {
-          if (! is_null($this->searchTypes)) {
-              return ['searchTypes' => $this->searchTypes->toArray()];
-          }
-
-          return new stdClass;
-      }
-  }
+        return new stdClass;
+    }
+}
