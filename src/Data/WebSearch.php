4037f8af1253c488c9b42a57031897a45fa98c71
Crypt0man3 <31904201+cryptoman3@users.noreply.github.com>
2026-02-26 17:13:21 +0000

    Create WebSearch.php

---
+++ src/Data/WebSearch.php
@@ -0,0 +1,30 @@
+ <?php                                                                                                                                                                                                                                                                                                                     
+                                                            
+  declare(strict_types=1);
+
+  namespace Gemini\Data;
+
+  use Gemini\Contracts\Arrayable;
+  use stdClass;
+
+  /**
+   * Web search tool type.
+   *
+   * https://ai.google.dev/api/caching#WebSearch
+   */
+  final class WebSearch implements Arrayable
+  {
+      public function __construct()
+      {
+      }
+
+      public static function from(): self
+      {
+          return new self;
+      }
+
+      public function toArray(): stdClass
+      {
+          return new stdClass;
+      }
+  }
