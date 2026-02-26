5a366c500c6c73947aa96d71eb92535531a06946
Crypt0man3 <31904201+cryptoman3@users.noreply.github.com>
2026-02-26 17:27:41 +0000

    Update ImageSearch.php

--- src/Data/ImageSearch.php
+++ src/Data/ImageSearch.php
@@ -1,30 +1,29 @@
 <?php
 
-  declare(strict_types=1);
+declare(strict_types=1);
 
-  namespace Gemini\Data;
+namespace Gemini\Data;
 
-  use Gemini\Contracts\Arrayable;
-  use stdClass;
+use stdClass;
 
-  /**
-   * Image search tool type.
-   *
-   * https://ai.google.dev/api/caching#ImageSearch
-   */
-  final class ImageSearch implements Arrayable
-  {
-      public function __construct()
-      {
-      }
+/**
+ * Image search tool type.
+ *
+ * https://ai.google.dev/api/caching#ImageSearch
+ */
+final class ImageSearch
+{
+    public function __construct()
+    {
+    }
 
-      public static function from(): self
-      {
-          return new self;
-      }
+    public static function from(): self
+    {
+        return new self;
+    }
 
-      public function toArray(): stdClass
-      {
-          return new stdClass;
-      }
-  }
+    public function toArray(): stdClass
+    {
+        return new stdClass;
+    }
+}
